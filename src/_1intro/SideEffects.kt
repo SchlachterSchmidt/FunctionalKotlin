e4fbc8ba087a7c18a77e1434584b3e480e62dd02
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-07-26 19:29:32 +0100

    lecture: side effects

---
+++ src/_1intro/SideEffects.kt
@@ -0,0 +1,21 @@
+package _1intro
+
+var global = 0
+
+fun main(args: Array<String>) {
+
+    println(first(1)) // prints 1
+    println(first(1)) // prints 2
+
+    println(second()) // prints 4
+    println(second()) // prints 4
+
+    println(first(2)) // prints 3
+    println(second()) // prints 6
+
+    // all these functions produce different output, even when called with the same input. they are impure
+}
+
+fun first(x: Int) = global++ + 1
+
+fun second() = global * 2
