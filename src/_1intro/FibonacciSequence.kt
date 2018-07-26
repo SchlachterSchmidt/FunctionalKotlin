f364882d308c9524626607a98eecd8f12ccee85e
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-07-26 19:30:37 +0100

    lecture: Fibonacci sequence

---
+++ src/_1intro/FibonacciSequence.kt
@@ -0,0 +1,16 @@
+package _1intro
+
+fun main(args: Array<String>) {
+    println(fib(0))
+    println(fib(1))
+    println(fib(2))
+    println(fib(3))
+    println(fib(4))
+    println(fib(5))
+}
+
+fun fib(n: Long): Long = when (n) {
+    0L -> 0L
+    1L -> 1L
+    else -> fib(n - 1) + fib(n - 2)
+}
