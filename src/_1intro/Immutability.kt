047855234335506568117965cbd8edaceb17836f
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-07-26 19:29:55 +0100

    lecture: Immutability

---
+++ src/_1intro/Immutability.kt
@@ -0,0 +1,12 @@
+package _1intro
+
+fun main(args: Array<String>) {
+
+    val john = Person("John", 22)
+    // cannot reassign age, but rather create a new object with different attributes. This is how we can manage state
+    john.copy(age = 23)
+    // johns age remains unchanged
+    println(john)
+}
+
+data class Person(val name: String, val age: Int)
