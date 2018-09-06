4464e1747dca0c87859a976010649b8c57c24f7b
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-09-06 12:13:49 +0100

    lecture: coroutines

--- src/_04sealedclasses_algebraictypes_coroutines/03Coroutines.kt
+++ src/_04sealedclasses_algebraictypes_coroutines/03Coroutines.kt
@@ -1,11 +1,36 @@
 package _04sealedclasses_algebraictypes_coroutines
 
-import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import java.lang.Thread.sleep
 
 fun main(args: Array<String>) {
 
-    val job = async {
-
+    val job1 = async {
         delay(1000)
+        "Hello"
     }
+
+    val job2 = async {
+        delay(1000)
+        "World"
+    }
+
+    // launch runs the jobs in a non-blocking way
+    launch {
+        val text = "${ job1.await() } ${ job2.await() }"
+        println(text)
+    }
+    // because launch is non-blocking, we need to suspend for a few seconds to see the output
+    sleep(3000)
+
+    // same as launch,  but blocking
+    runBlocking {
+        val text = "${ job1.await() } ${ job2.await() }"
+        println(text)
+    }
+
+
 }
